26b93456d0fbed1c3f12bf30ccbe7908c6641468
dsumitra <dsumitra@github.com>
2020-03-28 00:42:15 +0530

    Started Reports module and added .gitignore file

--- src/olx/reports/Reports.java
+++ src/olx/reports/Reports.java
@@ -1,8 +1,11 @@
 package olx.reports;
 
 public class Reports {
-	
+
 	void showReportOptions() {
+		System.out.println("Report Options");
+		System.out.println(" 1.Category-Classifieds Report\n 2.User-Classifieds Report\n "
+				+ "3.Classified Status Report\n 4.Sale Report\n 5.Exit Menu");
 		
 	}
 }
