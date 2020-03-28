4e07dd8675fa32f4e3accf36f3b2e36ca52d2e24
AnastasiaA17 <62458698+AnastasiaA17@users.noreply.github.com>
2020-03-29 03:14:39 +0530

    user test updated

--- src/olx/user/UserTest.java
+++ src/olx/user/UserTest.java
@@ -1,10 +1,10 @@
-package olx.user;
-
-import java.sql.SQLException;
-
-public class UserTest {
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		User u = new User();
-		u.login();
-	}
-}
+//package olx.user;
+//
+//import java.sql.SQLException;
+//
+//public class UserTest {
+//	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//		User u = new User();
+//		u.login();
+//	}
+//}
