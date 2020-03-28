a6b41b26b1de33d4bc8f740e89bbbf43940c9944
dsumitra <dsumitra@github.com>
2020-03-28 16:40:43 +0530

    classified edit

--- src/olx/classifieds/ClassifiedsTest.java
+++ src/olx/classifieds/ClassifiedsTest.java
@@ -1,36 +1,35 @@
 package olx.classifieds;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import olx.user.UserConstants;
 import olx.user.UserModel;
 
 public class ClassifiedsTest {
 	public static void main(String[] args) {
 
+//		UserModel user = new UserModel();
+//
 		UserModel user = new UserModel(1, "user", "usrLastName", "7895321890", "user@gmail.com", "address given",
-				false);
-		UserModel admin = new UserModel(3, "admin", "adminLastName", "1235664532", "admin@gmail.com", "address given",
-				true);
-		UserModel uModel = user;
+				false, UserConstants.UserStatus.ACTIVE);
+//		UserModel admin = new UserModel(3, "admin", "adminLastName", "1235664532", "admin@gmail.com", "address given",
+//				true);
 
 		Classifieds classified = new Classifieds();
-//   classified.buy(uModel);
-    classified.sellClassifieds(uModel);
-    
-//		classified.manageClassifieds(uModel);
+		classified.buy(user);
+		classified.sellClassifieds(user);
+
+		classified.manageClassifieds(user);
 		classified.displayPostedClassifieds();
-//		classified.addClassifieds(uModel);
-//		classified.updateClassified(1);
-//		classified.displayAllClassifieds(0);//passing this by Default.
-//		classified.displayClassifiedsByUser(1);
-// 		classified.deleteClassifieds(1);
+		classified.addClassifieds(user);
+		classified.updateClassified(user);
+//classified.displayAllClassifieds(0);//passing this by Default.
+//classified.displayClassifiedsByUser(uModel);
+//classified.deleteClassifieds(uModel);
 		// TODO: remove dummyIds and pass classifiedIds
 //		List<Integer> dummyIds = new ArrayList<>();
 //		dummyIds.add(121);
 //		dummyIds.add(122);
 //		classified.markClassifiedsAsSold(103);
 //		classified.buy(uModel);
-		
+
 	}
 }
